c3bcbfaab290bfaa4086ef3035fb1fe98aec7fe9
Arlindo Silva <arlindovicente@gmail.com>
2023-10-31 23:32:47 -0300

    Iniciando
    criando as dependências local e aws.

---
+++ src/main/java/com/learning/springboot/checklistapi/entity/ChecklistItemEntity.java
@@ -0,0 +1,31 @@
+package com.learning.springboot.checklistapi.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+@Entity(name = "ChecklistItem")
+@Table(indexes = { @Index(name = "IDX_GUID_CK_IT", columnList = "guid") })
+public class ChecklistItemEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long checklistItemId;
+
+    private String description;
+    private Boolean isCompleted;
+    private LocalTime dateEnd;
+    private LocalTime datePost;
+
+    @ManyToOne
+    private CategoryEntity category;
+}
