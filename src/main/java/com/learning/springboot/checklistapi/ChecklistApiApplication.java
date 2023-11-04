6faabf19c715a25c043ed69ba3db4f0ac76ef5db
Arlindo Silva <arlindovicente@gmail.com>
2023-11-03 22:39:22 -0300

    Swagger

--- src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
+++ src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
@@ -1,13 +1,27 @@
 package com.learning.springboot.checklistapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChecklistApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ChecklistApiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Checklist API")
+						.version("1.0.0")
+						.description("Checklist API v1.0.0")
+				);
 	}
 
 }
--- src/main/java/com/learning/springboot/checklistapi/controller/ChecklistItemController.java
+++ src/main/java/com/learning/springboot/checklistapi/controller/ChecklistItemController.java
@@ -3,6 +3,9 @@ package com.learning.springboot.checklistapi.controller;
 import com.learning.springboot.checklistapi.dto.ChecklistItemDTO;
 import com.learning.springboot.checklistapi.entity.ChecklistItemEntity;
 import com.learning.springboot.checklistapi.service.ChecklistItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -38,6 +41,11 @@ public class ChecklistItemController {
      * Retorna todos os itens de checklist cadastrados.
      * @return ResponseEntity<List<ChecklistItemDTO>> - Lista de itens de checklist.
      */
+    @Operation(description = "Retorna todos os itens de checklist cadastrados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Itens de checklist retornados com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro interno.")
+    })
     @GetMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ChecklistItemDTO>> getAllChecklistItems() {
 
@@ -53,6 +61,12 @@ public class ChecklistItemController {
      * @param checklistItemDTO - DTO com as informações do item de checklist a ser criado.
      * @return ResponseEntity<String> - GUID do item de checklist criado.
      */
+    @Operation(description = "Cria um novo item de checklist.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Itens de checklist criado com sucesso."),
+            @ApiResponse(responseCode = "422", description = "Category guid não foi encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno.")
+    })
     @PostMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> createNewChecklistItem(@RequestBody ChecklistItemDTO checklistItemDTO) {
 
@@ -75,6 +89,11 @@ public class ChecklistItemController {
      * @param checklistItemDTO - DTO com as informações do item de checklist a ser atualizado.
      * @return ResponseEntity<Void> - Resposta vazia com status NO_CONTENT.
      */
+    @Operation(description = "Atualiza um item de checklist existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Itens de checklist atualizado com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro interno.")
+    })
     @PutMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateChecklistItem(@RequestBody ChecklistItemDTO checklistItemDTO) {
 
@@ -99,6 +118,11 @@ public class ChecklistItemController {
      * @param guid - GUID do item de checklist a ser deletado.
      * @return ResponseEntity<Void> - Resposta vazia com status NO_CONTENT.
      */
+    @Operation(description = "Deleta um item de checklist existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Itens de checklist deletado com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro interno.")
+    })
     @DeleteMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> deteleChecklistItem(@PathVariable String guid){
         this.checklistItemService.deleteChecklistItem(guid);
