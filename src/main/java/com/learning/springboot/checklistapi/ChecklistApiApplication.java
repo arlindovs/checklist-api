ce976301a953d0ff6b789b1d74353114aa09e477
Arlindo Silva <arlindovicente@gmail.com>
2023-11-03 23:16:49 -0300

    Update ChecklistApiApplication.java

--- src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
+++ src/main/java/com/learning/springboot/checklistapi/ChecklistApiApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChecklistApiApplication {
@@ -12,6 +15,34 @@ public class ChecklistApiApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ChecklistApiApplication.class, args);
+	}
+
+	@Profile("local")
+	@Bean
+	public WebMvcConfigurer corsLocalConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Origin","X-Requested-With", "Content-Type", "Accept", "Authorization")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+
+	@Profile("aws")
+	@Bean
+	public WebMvcConfigurer corsAwsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Origin","X-Requested-With", "Content-Type", "Accept", "Authorization")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 	@Bean
